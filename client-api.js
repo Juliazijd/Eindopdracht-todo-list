15d66a26c1f79a271726ce6a2b9a7d476ff8426b
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-04-16 15:56:15 +0200

    Added function to add task, delete task, scratch task with checkbox and added CSS styling

--- client-api.js
+++ client-api.js
@@ -1,18 +1,42 @@
-const getTodoList = async () => {
-    const url = 'http://localhost:3000/';
-	const data = {description: "buy oatmeal", done: false};
-	const res = await fetch(url, {
-		method: "POST",
-		body: JSON.stringify(data),
-		headers: { 
-			"Content-Type": "application/json", 
-		},
-	});
-	const json = await res.json();
-	return json;
-}
-const showResult = async (data) => console.log(data);
+const taskInput = document.querySelector('form');
+const taskList = document.getElementById('task-list');
 
-document.querySelector("button").addEventListener("click", () => {
-getTodoList().then(showResult);
+const getTask = async () => {
+    const url = 'http://localhost:3000/';
+    const task = {description: document.querySelector('#add-task-input').value, done: false};
+    const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { 
+            "Content-Type": "application/json", 
+        },
+    });
+    const json = await res.json();
+    return json;
+}
+const deleteTask = event => event.target.parentNode.remove();
+const scratchTask = event => event.target.nextSibling.classList.toggle('scratch');
+const addTask = task => {
+  let listItem = document.createElement("li");
+  let checkBox = document.createElement("input");
+  let label = document.createElement("label");
+  let deleteButton = document.createElement("button");
+  
+  checkBox.type = "checkbox";
+  checkBox.className = "checkbox";
+  deleteButton.innerText = "🗑";
+  deleteButton.className = "delete";
+  label.innerText = task.description;
+  label.className = "task";
+
+  deleteButton.addEventListener("click", deleteTask);
+  checkBox.addEventListener("change", scratchTask)
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(deleteButton);
+  taskList.append(listItem);
+}
+taskInput.addEventListener('submit', event => {
+	event.preventDefault();
+    getTask().then(addTask);
 });
